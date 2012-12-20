063bca74698bc992619744993664c35ecd1057ae
frycoo <piotr.fryc@gmail.com>
2012-12-20 04:08:19 -0800

    Man hour sum

--- app/controllers/raport_controller.rb
+++ app/controllers/raport_controller.rb
@@ -1,6 +1,10 @@
 class RaportController < ApplicationController
   def monthly
-		@mydate = Date.new(params[:date][:year].to_i, params[:date][:month].to_i, params[:date][:day].to_i).beginning_of_month
+		if params[:date] == nil
+			@mydate = Date.current.beginning_of_month
+		else
+			@mydate = Date.new(params[:date][:year].to_i, params[:date][:month].to_i, params[:date][:day].to_i).beginning_of_month
+		end
 		@employees = Employee.all 
   end
 end
