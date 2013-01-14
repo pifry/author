b9e4faaac2fa37faf516d7956a130c039b248ad7
frycoo <piotr.fryc@gmail.com>
2013-01-14 02:45:54 -0800

    more department_id modifications

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,5 +2,5 @@ class Employee < ActiveRecord::Base
 	has_many :assignments
 	has_many :projects, :through => :assignments
 
-  attr_accessible :name, :number
+  attr_accessible :name, :number, :department_id
 end
